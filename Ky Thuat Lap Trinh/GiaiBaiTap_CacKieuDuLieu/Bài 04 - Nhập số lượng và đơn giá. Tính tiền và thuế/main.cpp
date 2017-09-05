67c14eedeaec78964ffd7e2dc18a0af44376750c
Le Hong Hai <mail.lehonghai@gmail.com>
2017-09-05 18:33:19 +0700

    KTLT - Bai tap Cac kieu du lieu co so

---
+++ Ky Thuat Lap Trinh/GiaiBaiTap_CacKieuDuLieu/Bài 04 - Nhập số lượng và đơn giá. Tính tiền và thuế/main.cpp
@@ -0,0 +1,29 @@
+﻿/*
+- Nhập số lượng và đơn giá của một sản phẩm
+- Tính tiền và thuế giá trị gia tăng (GTGT)
+- Biết:
+	Tiền = số lượng * đơn giá
+	Thuế GTGT = 10% tiền
+*/
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+	unsigned int SoLuong, DonGia;
+
+	printf("Nhap so luong: ");
+	scanf("%u", &SoLuong);
+	printf("Nhap don gia: ");
+	scanf("%u", &DonGia);
+
+	unsigned int Tien = SoLuong * DonGia;
+	float Thue = Tien *0.1;
+
+	printf("-------------\n");
+	printf("Tien = %.2u $\n", Tien);
+	printf("Thue = %.2f $\n", Thue);
+
+	getch();
+	return 0;
+}
---
+++ Ky Thuat Lap Trinh/GiaiBaiTap_CacKieuDuLieu/Bài 06 - Tính chu vi và diện tích hình tròn/main.cpp
@@ -0,0 +1,24 @@
+﻿/*
+- Nhập vào bán kính của hình tròn
+- Tính và in ra chu vi, diện tích của hình tròn
+*/
+#include <stdio.h>
+#include <conio.h>
+
+#define PI 3.14159
+
+int main()
+{
+	float BanhKinh, ChuVi, DienTich;
+	printf("Nhap ban kinh = ");
+	scanf("%f", &BanhKinh);
+
+	ChuVi = 2 * BanhKinh * PI;
+	DienTich = BanhKinh * BanhKinh * PI;
+
+	printf("Chu vi: %.3f\n", ChuVi);
+	printf("Dien tich: %.3f\n", DienTich);
+
+	getch();
+	return 0;
+}
